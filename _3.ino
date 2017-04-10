51254ca5d1bca519fb536468ad60bf8c78103649
vernier04053096 <vernier123@gmail.com>
2017-04-10 17:48:36 +0800

    Add files via upload

---
+++ _3.ino
@@ -0,0 +1,78 @@
+const byte LEDs[] = {0,1,2,3,4,5,6,7};
+const byte total = sizeof(LEDs);
+const byte button=13;
+
+
+void setup(){
+// ±N¨C­Ó Pin ³]©w¦¨¿é¥X
+for (byte i=0;i<total;i++) {
+pinMode(LEDs[i], OUTPUT);
+}
+  pinMode(button, INPUT);
+    for(int i=0;i<8;i++)
+  {
+    digitalWrite(LEDs[i],LOW);
+
+    
+  }
+}
+
+void loop() {
+
+ 
+  if(digitalRead(button)==0)
+  {
+  
+     
+  	for(int i=0;i<8;i++)
+  	{
+      if(digitalRead(button)==0){
+    	digitalWrite(LEDs[i],HIGH);
+    	delay(500);
+     	digitalWrite(LEDs[i],LOW);
+      }
+      else
+      {
+      	break;
+      }
+      
+ 	 }
+
+ 	 for(int i=8;i>=0;--i)
+  	{
+       if(digitalRead(button)==0){
+   	 	digitalWrite(LEDs[i],HIGH);
+   		delay(500);
+    	digitalWrite(LEDs[i],LOW);
+       }
+       else
+       {
+       	break;
+       }
+  	}
+  }
+  else
+  {
+  for(int i=8;i>=0;--i)
+  {
+    digitalWrite(LEDs[i],HIGH);
+  }
+    delay(500);
+    for(int i=8;i>=0;--i)
+  {
+    digitalWrite(LEDs[i],LOW);
+  }
+    delay(500);
+    for(int i=8;i>=0;--i)
+  {
+    digitalWrite(LEDs[i],HIGH);
+  }
+    delay(500);
+    for(int i=8;i>=0;--i)
+  {
+    digitalWrite(LEDs[i],LOW);
+  }
+    delay(500);
+  
+  }
+ }
